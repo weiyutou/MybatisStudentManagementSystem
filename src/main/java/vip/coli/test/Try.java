293b8839118970ad6c04031f131bcd986bbd6ce6
HecoliPRO <3119813955@qq.com>
2023-03-29 20:16:46 +0800

    update

--- src/main/java/vip/coli/test/Try.java
+++ src/main/java/vip/coli/test/Try.java
@@ -4,5 +4,9 @@ package vip.coli.test;
  * @author coli
  * @data 2023/3/29
  */
+
 public class Try {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
